4d0bab7c14e49ec328c5b01e6ab398f31b2ea5c1
Tabishdehlvi <tabishdehlvi3@gmail.com>
2024-05-15 13:24:28 +0500

    bug fixes 2

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -5,4 +5,4 @@ import { AuthContext } from 'src/context/auth'
 
 type AuthContextType = JwtAuthContextType
 
-export const useAuth = <T = AuthContextType>() => useContext(AuthContext) as T
+export const useAuth = <T = AuthContextType>() => useContext(AuthContext) as unknown as T
