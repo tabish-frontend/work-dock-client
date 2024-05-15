25c7754f203170f9cc4521ed41611c614e37c1e7
Tabishdehlvi <tabishdehlvi3@gmail.com>
2024-05-15 13:14:01 +0500

    bug fixes

--- src/formilk/initial-values.ts
+++ src/formilk/initial-values.ts
@@ -4,26 +4,3 @@ export const LoginInitialValues: Login = {
   email: 'johndoe',
   password: 'John12385'
 }
-
-export const EmployeeInitialValues: Employee = {
-  _id: '',
-  username: '',
-  full_name: '',
-  role: '',
-  avatar: '',
-  bio: '',
-  account_status: '',
-  company: '',
-  mobile: 0,
-  natinal_identity_number: '',
-  email: '',
-  gender: '',
-  qualification: '',
-  country: '',
-  dob: '',
-  join_date: null,
-  leave_date: null,
-  languages: [],
-  designation: [],
-  qualification_certificates: []
-}
